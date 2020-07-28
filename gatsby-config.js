76b40497857513f20be6e329930e81c99651f057
olethrosjv <chefjoevaughn@gmail.com>
2020-07-27 18:02:17 -0700

    first time

--- gatsby-config.js
+++ gatsby-config.js
@@ -110,6 +110,26 @@ module.exports = {
       }
     },
     {
+      resolve: `gatsby-source-wordpress`,
+      options: {
+        /*
+         * The base URL of the WordPress site without the trailingslash and the protocol. This is required.
+         * Example : 'dev-gatbsyjswp.pantheonsite.io' or 'www.example-site.com'
+         */
+        baseUrl: `http://joevau4.freehostia.com/joevaughn`,
+        // The protocol. This can be http or https.
+        protocol: `http`,
+        // Indicates whether the site is hosted on wordpress.com.
+        // If false, then the assumption is made that the site is self hosted.
+        // If true, then the plugin will source its content on wordpress.com using the JSON REST API V2.
+        // If your site is hosted on wordpress.com, then set this to true.
+        hostingWPCOM: false,
+        // If useACF is true, then the source plugin will try to import the WordPress ACF Plugin contents.
+        // This feature is untested for sites hosted on WordPress.com
+        useACF: false,
+      },
+    },
+    {
       resolve: "gatsby-plugin-feed",
       options: {
         setup(ref) {
